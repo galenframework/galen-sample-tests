df8070e4248b47a5482f27e7b8e01fd2f9d17923
Ivan Shubin <ivan.ishubin@gmail.com>
2016-02-28 22:07:44 +0100

    Updated specs to support changes in galen-extras lib

--- specs/galen-extras/galen-extras-rules.js
+++ specs/galen-extras/galen-extras-rules.js
@@ -1,6 +1,6 @@
 
 if (this.GEXTRAS_NO_MARGIN === undefined || this.GEXTRAS_NO_MARGIN === null) {
-    this.GEXTRAS_NO_MARGIN = "0px";
+    this.GEXTRAS_NO_MARGIN = "-1 to 1px";
 }
 
 function _ruleRenderedInTable(rule, itemPattern, columns, verticalMargin, horizontalMargin) {
@@ -44,6 +44,17 @@ rule("%{itemPattern} are rendered in %{columns: [0-9]+} column table layout", fu
 
 
 /**
+ * This is a high-level spec for checking that elements are displayed in table layout
+ * e.g.
+ *
+ *      | menuItem-* are rendered in 2 column table layout, with 0 to 4px margin
+ */
+rule("%{itemPattern} are rendered in %{columns: [0-9]+} column table layout, with %{margin} margin", function (objectName, parameters) {
+    _ruleRenderedInTable(this, parameters.itemPattern, parseInt(columns), parameters.margin, parameters.margin);
+});
+
+
+/**
  * This is a high-level spec for checking that elements are displayed in table layout 
  * with different margins for vertical and horizontal sides
  * e.g.
@@ -55,22 +66,11 @@ rule("%{itemPattern} are rendered in %{columns: [0-9]+} column table layout, wit
 });
 
 
-/**
- * This is a high-level spec for checking that elements are displayed in table layout
- * e.g.
- *
- *      | menuItem-* are rendered in 2 column table layout, with 0 to 4px margin
- */
-rule("%{itemPattern} are rendered in %{columns: [0-9]+} column table layout, with %{margin} margin", function (objectName, parameters) {
-    _ruleRenderedInTable(this, parameters.itemPattern, parseInt(columns), parameters.margin, parameters.margin);
-});
-
-
-function _applyRuleBodyForAllElements(rule, objectPattern, appliesConditionCallback) {
+function _applyRuleBodyForAllElements(rule, parameters, appliesConditionCallback) {
     var allElements = findAll(parameters.objectPattern);
 
     if (allElements.length > 0) {
-        for (var i = 0; i < allElements.length - 1; i += 1) {
+        for (var i = 0; i < allElements.length; i += 1) {
             if (!appliesConditionCallback(allElements[i])) {
                 return;
             }
@@ -79,11 +79,11 @@ function _applyRuleBodyForAllElements(rule, objectPattern, appliesConditionCallb
     }
 }
 
-function _applyRuleBodyForSingleElement(rule, objectPattern, appliesConditionCallback) {
+function _applyRuleBodyForSingleElement(rule, parameters, appliesConditionCallback) {
     var allElements = findAll(parameters.objectPattern);
 
     if (allElements.length > 0) {
-        for (var i = 0; i < allElements.length - 1; i += 1) {
+        for (var i = 0; i < allElements.length; i += 1) {
             if (appliesConditionCallback(allElements[i])) {
                 rule.doRuleBody();
                 return;
@@ -93,20 +93,20 @@ function _applyRuleBodyForSingleElement(rule, objectPattern, appliesConditionCal
 }
 
 rule("if all %{objectPattern} are visible", function (objectName, parameters) {
-    _applyRuleBodyForAllElements(this, parameters.objectPattern, function (element) {
+    _applyRuleBodyForAllElements(this, parameters, function (element) {
         return element.isVisible();
     });
 });
 
 
 rule("if none of %{objectPattern} are visible", function (objectName, parameters) {
-    _applyRuleBodyForAllElements(this, parameters.objectPattern, function (element) {
+    _applyRuleBodyForAllElements(this, parameters, function (element) {
         return ! element.isVisible();
     });
 });
 
 rule("if any of %{objectPattern} is visible", function (objectName, parameters) {
-    _applyRuleBodyForSingleElement(this, parameters.objectPattern, function (element) {
+    _applyRuleBodyForSingleElement(this, parameters, function (element) {
         return element.isVisible();
     });
 });
@@ -128,6 +128,20 @@ rule("%{objectPattern} sides are inside %{containerObject} with %{margin} margin
 });
 
 
+rule("%{objectPattern} sides are vertically inside %{containerObject}", function (objectName, parameters) {
+    var items = findAll(parameters.objectPattern);
+    if (items.length > 0) {
+        this.addObjectSpecs(items[0].name, [ "inside " + parameters.containerObject + " -1 to 1 px top" ]);
+        
+        for (var i = 1; i < items.length - 1; i++) {
+            this.addObjectSpecs(items[i].name, [ "inside " + parameters.containerObject ]);
+        }
+
+        this.addObjectSpecs(items[items.length - 1].name, [ "inside " + parameters.containerObject + " -1 to 1 px bottom" ]);
+    } else {
+        throw new Error("Couldn't find any items matching " + parameters.objectPattern);
+    }
+});
 
 rule("%{objectPattern} sides are vertically inside %{containerObject} with %{margin} margin", function (objectName, parameters) {
     var items = findAll(parameters.objectPattern);
@@ -145,16 +159,16 @@ rule("%{objectPattern} sides are vertically inside %{containerObject} with %{mar
 });
 
 
-rule("%{objectPattern} sides are vertically inside %{containerObject}", function (objectName, parameters) {
+rule("%{objectPattern} sides are horizontally inside %{containerObject}", function (objectName, parameters) {
     var items = findAll(parameters.objectPattern);
     if (items.length > 0) {
-        this.addObjectSpecs(items[0].name, [ "inside " + parameters.containerObject + " 0 to 1 px top" ]);
+        this.addObjectSpecs(items[0].name, [ "inside " + parameters.containerObject + " -1 to 1 px left" ]);
         
         for (var i = 1; i < items.length - 1; i++) {
             this.addObjectSpecs(items[i].name, [ "inside " + parameters.containerObject ]);
         }
 
-        this.addObjectSpecs(items[items.length - 1].name, [ "inside " + parameters.containerObject + " 0 to 1 px bottom" ]);
+        this.addObjectSpecs(items[items.length - 1].name, [ "inside " + parameters.containerObject + " -1 to 1 px right" ]);
     } else {
         throw new Error("Couldn't find any items matching " + parameters.objectPattern);
     }
@@ -171,22 +185,6 @@ rule("%{objectPattern} sides are horizontally inside %{containerObject} with %{m
         }
 
         this.addObjectSpecs(items[items.length - 1].name, [ "inside " + parameters.containerObject + " " + parameters.margin + " right" ]);
-    } else {
-        throw new Error("Couldn't find any items matching " + parameters.objectPattern);
-    }
-});
-
-
-rule("%{objectPattern} sides are horizontally inside %{containerObject}", function (objectName, parameters) {
-    var items = findAll(parameters.objectPattern);
-    if (items.length > 0) {
-        this.addObjectSpecs(items[0].name, [ "inside " + parameters.containerObject + " 0 to 1 px left" ]);
-        
-        for (var i = 1; i < items.length - 1; i++) {
-            this.addObjectSpecs(items[i].name, [ "inside " + parameters.containerObject ]);
-        }
-
-        this.addObjectSpecs(items[items.length - 1].name, [ "inside " + parameters.containerObject + " 0 to 1 px right" ]);
     } else {
         throw new Error("Couldn't find any items matching " + parameters.objectPattern);
     }
